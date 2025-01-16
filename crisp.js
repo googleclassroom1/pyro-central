060da02da9a9eec44bfee3097a1d163e12404e8a
Pyro <doubletooksplit@outlook.com>
2025-01-16 22:28:49 +0000

    Create crisp.js

---
+++ crisp.js
@@ -0,0 +1,9 @@
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "98006de2-e6a4-482c-bea7-75cd2e199619";
+(function() {
+    d = document;
+    s = d.createElement("script");
+    s.src = "https://client.crisp.chat/l.js";
+    s.async = 1;
+    d.getElementsByTagName("head")[0].appendChild(s);
+})();
