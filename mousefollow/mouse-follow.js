f4383c33511354d89726f6cd730d3265aed215c8
Pyro <doubletooksplit@outlook.com>
2025-01-20 21:20:12 +0000

    Update mouse-follow.js

--- mousefollow/mouse-follow.js
+++ mousefollow/mouse-follow.js
@@ -1,7 +1,7 @@
 document.addEventListener('mousemove', function(e) {
     const freddy = document.getElementById('freddy');
     if (freddy) {
-        freddy.style.left = e.pageX + 'px';
-        freddy.style.top = e.pageY + 'px';
+        freddy.style.left = (e.pageX - freddy.offsetWidth / 2) + 'px';
+        freddy.style.top = (e.pageY - freddy.offsetHeight / 2) + 'px';
     }
 });
