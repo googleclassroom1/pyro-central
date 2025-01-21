a6caec93ac9c5c3e5ab8aee945eeb19a845d0040
Pyro <doubletooksplit@outlook.com>
2025-01-21 06:56:34 +0000

    Update mouse-follow.js

--- mousefollow/mouse-follow.js
+++ mousefollow/mouse-follow.js
@@ -1,6 +1,7 @@
-documentdocument.addEventListener('mousemove', function(e) {
+document.addEventListener('mousemove', function(e) {
     const freddy = document.getElementById('freddy');
     if (freddy) {
+        console.log('Mouse moved:', e.clientX, e.clientY); // Add this line
         freddy.style.left = (e.clientX - freddy.offsetWidth / 2) + 'px';
         freddy.style.top = (e.clientY - freddy.offsetHeight / 2) + 'px';
     }
