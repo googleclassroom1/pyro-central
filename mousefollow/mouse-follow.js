37f93197399db6e70a89ae65a4ef3bb7ff19b0c0
Pyro <doubletooksplit@outlook.com>
2025-01-24 07:13:01 +0000

    Update mouse-follow.js

--- mousefollow/mouse-follow.js
+++ mousefollow/mouse-follow.js
@@ -1,7 +1,9 @@
 document.addEventListener('mousemove', function(e) {
     const freddy = document.getElementById('freddy');
     if (freddy) {
-        console.log('Mouse moved:', e.clientX, e.clientY); // Add this line
+        freddy.style.backgroundImage = 'url("/images/cursor.png")';
+        freddy.style.backgroundSize = 'contain';
+        freddy.style.backgroundRepeat = 'no-repeat';
         freddy.style.left = (e.clientX - freddy.offsetWidth / 2) + 'px';
         freddy.style.top = (e.clientY - freddy.offsetHeight / 2) + 'px';
     }
