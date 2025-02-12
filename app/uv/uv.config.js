286e19e3a66d6925fdfab8c45c051f767fa72243
Pyro <doubletooksplit@outlook.com>
2025-02-12 19:55:44 +0000

    Update uv.config.js

--- app/uv/uv.config.js
+++ app/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/app/login/',
-    bare: "https://bare.benrogo.net/",
+    bare: "https://sysadmin.us.kg/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/app/uv/uv.handler.js',
